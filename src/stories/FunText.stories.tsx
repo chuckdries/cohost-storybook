2117facdd91fa50650d9105af9dc963d42181c81
Chuck Dries <chuck@chuckdries.com>
2022-07-03 04:02:01 -0700

    add "Fun Text" story

---
+++ src/stories/FunText.stories.tsx
@@ -0,0 +1,77 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { CSSProperties } from "react";
+import StoryTemplate, { StoryTemplateProps } from "../StoryTemplate";
+
+const h1Style: CSSProperties = {
+  fontSize: "6em",
+  margin: 0,
+  position: "relative",
+};
+
+const Post = (props: StoryTemplateProps) => (
+  <StoryTemplate {...props}>
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <h1
+        style={{ ...h1Style, transform: "rotate(-30deg)", alignSelf: "center" }}
+      >
+        the
+      </h1>
+      <h1 style={{ ...h1Style, transform: "rotate(-15deg)" }}>ULTIMATE</h1>
+      <h1
+        style={{
+          ...h1Style,
+          transform: "rotate(15deg)",
+          alignSelf: "end",
+          top: "3rem",
+          color: "cadetblue",
+        }}
+      >
+        POSTING
+      </h1>
+      <h1 style={{ ...h1Style, transform: "rotate(-5deg)", left: "4rem" }}>
+        TOOL
+      </h1>
+      <a style={{ alignSelf: "center" }} href="https://cohost-storybook.netlify.app/">
+        <h2
+          style={{
+            border: "3px solid cadetblue",
+            padding: "1em",
+            display: "inline-block",
+            textDecoration: 'underline',
+            textDecorationColor: 'cadetblue'
+          }}
+        >
+          Discover the IDE for chosts
+        </h2>
+      </a>
+    </div>
+  </StoryTemplate>
+);
+
+export default {
+  title: "Posts/Fun Text", // <--- EDIT THIS
+  component: Post,
+  argTypes: {
+    wrapWithPosts: {
+      name: "Wrap with posts",
+      type: "boolean",
+      defaultValue: true,
+    },
+    showHTML: {
+      name: "Show HTML",
+      type: "boolean",
+      defaultValue: false,
+    },
+  },
+} as ComponentMeta<typeof Post>;
+
+const Template: ComponentStory<typeof Post> = (args) => <Post {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};

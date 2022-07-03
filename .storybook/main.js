72adee0e8d9497de3812e313b6de6b63f14d3634
Chuck Dries <chuck@chuckdries.com>
2022-07-03 05:47:15 -0700

    add story source panel to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,16 @@
 module.exports = {
-  stories: [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        actions: false,
+      },
+    },
+    // "@storybook/addon-interactions",
     "storybook-dark-mode",
+    "@storybook/addon-storysource",
   ],
   framework: "@storybook/react",
   core: {
@@ -17,10 +20,9 @@ module.exports = {
     storyStoreV7: true,
   },
   async viteFinal(config, { configType }) {
-    if(configType === "PRODUCTION") {
-      return {...config, base: './'};
+    if (configType === "PRODUCTION") {
+      return { ...config, base: "./" };
     }
     return config;
-  }
-
+  },
 };

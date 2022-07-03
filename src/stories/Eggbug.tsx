271f4baae04f0badc398c8808b4b6e781564f827
Chuck Dries <chuck@chuckdries.com>
2022-07-03 06:08:59 -0700

    add eggbug to welcome note

---
+++ src/stories/Eggbug.tsx
@@ -0,0 +1,12 @@
+export default () => (
+  <img
+    src="https://cohost.org/static/41454e429d62b5cb7963.png"
+    style={{
+      display: 'inline',
+      width: "1.2em",
+      background: "white",
+      padding: ".15em",
+      borderRadius: "6px",
+    }}
+  />
+);
